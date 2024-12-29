a935a0b4e62f4153fe1cc0f39ca683ac347589f7
RainPlays09 <jhanreuell9@gmail.com>
2024-12-29 15:38:31 +0800

    commit

--- App.tsx
+++ App.tsx
@@ -1,9 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Button, View, Image, Text } from 'react-native';
+import * as VideoThumbnails from 'expo-video-thumbnails';
 
 export default function App() {
+  const [image, setImage] = useState(null);
+
+  const generateThumbnail = async () => {
+    try {
+      const { uri } = await VideoThumbnails.getThumbnailAsync(
+        'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+        {
+          time: 15000,
+        }
+      );
+      setImage(uri);
+    } catch (e) {
+      console.warn(e);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Button onPress={generateThumbnail} title="Generate thumbnail" />
+      {image && <Image source={{ uri: image }} style={styles.image} />}
+      <Text>{image}</Text>
     </View>
   );
 }
@@ -11,8 +31,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  image: {
+    width: 200,
+    height: 200,
   },
 });
